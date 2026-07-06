d00af449bf14e86e1748333ee0ef7026bb82f4c6
maverickgdn03 <u19004232@tuks.co.za>
2026-07-06 17:50:04 +0200

    SSR

--- indomisa-website/src/app/app.config.ts
+++ indomisa-website/src/app/app.config.ts
@@ -3,11 +3,11 @@ import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes, withInMemoryScrolling({
     scrollPositionRestoration: 'top',
   }
-  )), provideClientHydration(), provideHttpClient()]
+  )), provideClientHydration(), provideHttpClient(withFetch())],
 };
